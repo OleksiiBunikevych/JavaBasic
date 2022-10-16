ce3bd4820f1734477e136e58484b059b7eab56cd
Oleksii Bunikevych <oleksii.bunikevych@gmail.com>
2022-10-17 00:07:13 +0300

    виконав завдання №5 уроку №2

---
+++ src/lesson02/Task05.java
@@ -0,0 +1,22 @@
+package lesson02;
+
+import java.util.Scanner;
+
+public class Task05 {
+    //programma perovodu inches to centimeters;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double inches;
+        System.out.print("Vvedit znachennya inches: " + "inches=");
+        inches = scanner.nextDouble();
+        double centimeters = convertToCentimeters(inches);
+        System.out.print("centimeters=: " + centimeters);
+    }
+
+    public static double convertToCentimeters(double inches) {
+        double centimeters = 0;
+        centimeters = inches * 2.54;
+        return centimeters;
+    }
+}
+
