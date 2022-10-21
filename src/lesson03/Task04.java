ac11b335d28962551f7a66dd3602a4e42e872180
Oleksii Bunikevych <oleksii.bunikevych@gmail.com>
2022-10-21 19:18:25 +0300

    виконав завдання №4 уроку №3.

---
+++ src/lesson03/Task04.java
@@ -0,0 +1,14 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Task04 {
+    public static void main(String[] args) {
+        int summa = 0, i;
+        for (i = 0; i < 10; i++) {
+            summa = summa + i;
+        }
+        System.out.print("summa=" + summa);
+    }
+}
+
