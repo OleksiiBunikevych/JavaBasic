3c03835e47a2217a9c85e7989f3a2c35e37320ff
Oleksii Bunikevych <oleksii.bunikevych@gmail.com>
2022-10-23 16:39:15 +0300

    виконав завдання №3 уроку №6

---
+++ src/lesson06/Dog.java
@@ -0,0 +1,20 @@
+package lesson06;
+
+public class Dog {
+    private String name;
+    private int age;
+    private float weight;
+    private String color;
+
+    public void setValues(String name, int age, float weight, String color) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getValues() {
+        String info = "Dog name: " + name + "; age: " + age + "; weight: " + weight + "; color: " + color + ".";
+        return info;
+    }
+}
