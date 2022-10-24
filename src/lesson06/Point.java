b71bf48cdb394b760af2d45d41b9ca79713765b6
Oleksii Bunikevych <oleksii.bunikevych@gmail.com>
2022-10-24 20:18:10 +0300

    виправив завдання №4 уроку №6 (добавив get)

--- src/lesson06/Point.java
+++ src/lesson06/Point.java
@@ -8,6 +8,12 @@ public class Point {
         this.X = X;
         this.Y = Y;
     }
+    public int getX(){
+        return X;
+    }
+    public int getY(){
+        return Y;
+    }
     public double getDistanse(Point point) {
        return Math.sqrt(Math.pow(this.X-point.X, 2) + Math.pow(this.Y - point.Y, 2));
     }}
