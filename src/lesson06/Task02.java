9940dfc08d0cb32ac32fa9d6a8871d8224423dea
Oleksii Bunikevych <oleksii.bunikevych@gmail.com>
2022-10-24 20:39:41 +0300

    виправив завдання №2 уроку №6 (добавив get)

--- src/lesson06/Task02.java
+++ src/lesson06/Task02.java
@@ -2,9 +2,7 @@ package lesson06;
 
 public class Task02 {
     public static void main(String[] args) {
-        Circle kolo = new Circle();
-        final double Pi = 3.1415926536;
-        kolo.radius = 10;
+        Circle kolo = new Circle(3.1415926536, 10);
         System.out.println("area: " + kolo.getArea());
         System.out.println("perimetr: " + kolo.getPerimetr());
     }
