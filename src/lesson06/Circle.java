2e5ffac20355228ac7dedc94e06393646afb5675
Oleksii Bunikevych <oleksii.bunikevych@gmail.com>
2022-10-27 21:24:00 +0300

    виправив завдання №2 уроку №6 (добавив set/get)

--- src/lesson06/Circle.java
+++ src/lesson06/Circle.java
@@ -9,6 +9,15 @@ public class Circle {
         this.radius = radius;
     }
 
+    public double getRadius() {
+
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
     public double getArea() {
         double area = PI * Math.pow(radius, 2);
         return area;
